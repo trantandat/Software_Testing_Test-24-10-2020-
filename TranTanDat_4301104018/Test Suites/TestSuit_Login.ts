e6e17daecebaea4498a5a1e5006a9c1cd94e4044
trantandat <tandat180599@gmail.com>
2020-10-24 17:40:54 +0700

    TranTanDat_4301104018
    Bài test ngày 24/10/2020

---
+++ TranTanDat_4301104018/Test Suites/TestSuit_Login.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuit_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>tandat18059999@gmail.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>31245f60-6b2a-48db-8650-bf9acbd7b822</testSuiteGuid>
+   <testCaseLink>
+      <guid>f02fe4d5-1671-408f-a8cd-2c8a508fa8d0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase_2_TranTanDat</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8366110f-221c-4b4e-86cf-d2e5a6038bcd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase_1_TranTanDat</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>37998a87-e732-4f5c-808a-aeedaad6edb4</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
